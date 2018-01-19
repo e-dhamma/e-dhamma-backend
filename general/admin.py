1b4a19bae40cc58fed5f1a7a797dde597ea7f688
Eerik Sven Puudist <eerik.sven@hotmail.com>
2018-01-19 23:15:27 +0200

    Add admin view for LetterToAdmin

--- general/admin.py
+++ general/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import LetterToAdmin
 
-# Register your models here.
+class LetterToAdminAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'title', 'solved')
+
+admin.site.register(LetterToAdmin, LetterToAdminAdmin)
--- general/models.py
+++ general/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class LetterToAdmin (models.Model):
     name = models.CharField(max_length=250)
     email = models.EmailField()
+    title = models.CharField(max_length=250)
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     solved = models.BooleanField(default=False)
