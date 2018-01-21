011be377bcff3a5cecff6a6021617396229ce633
Eerik Sven Puudist <eerik.sven@hotmail.com>
2018-01-21 23:05:30 +0200

    For building admin site added third party library, which enables inlines in inlines

--- dhamma_backend/settings.py
+++ dhamma_backend/settings.py
@@ -31,13 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party
     'rest_framework',
+    'nested_inline',
+    # Personal
     'general',
     'term',
 ]
--- term/admin.py
+++ term/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin
 
-from .models import Term
-from .models import Comment
-from .models import TranslatorsChat
+from .models import Term, Pali, Meaning, Est, Eng, Example, Comment, TranslatorsChat
 
+class PaliAdmin(admin.TabularInline):
+    model = Pali
+    extra = 1
+class ExampleAdmin(NestedStackedInline):
+    model = Example
+    extra = 1
+class EngAdmin(NestedStackedInline):
+    model = Eng
+    extra = 1
+class EstAdmin(NestedStackedInline):
+    model = Est
+    extra = 1
 
+class MeaningAdmin(NestedStackedInline):
+    model = Meaning
+    extra = 1
+    inlines = (EstAdmin, EngAdmin, ExampleAdmin)
 class CommentAdmin(admin.TabularInline):
     model = Comment
     extra = 1
@@ -15,10 +30,10 @@ class TranslatorsChatAdmin(admin.TabularInline):
     extra = 1
 
 
-class TermAdmin(admin.ModelAdmin):
+class TermAdmin(NestedModelAdmin):
     save_on_top = True
     search_fields = ('id',)
-    inlines = (CommentAdmin, TranslatorsChatAdmin)
+    inlines = (PaliAdmin, MeaningAdmin, CommentAdmin, TranslatorsChatAdmin)
 
 
 admin.site.register(Term, TermAdmin)
