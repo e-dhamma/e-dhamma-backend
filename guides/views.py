ed97bc6e639e57348ecb842b2d003a9773128d85
Eerik Sven Puudist <eerik.sven@hotmail.com>
2018-02-07 15:24:57 +0200

    Add markdown reader

--- guides/views.py
+++ guides/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from markdown import markdown
 from .models import Guide
 
 def guide(request, header):
-    guide = Guide.objects.filter(header=header)
-    return render(request, 'guides/guide.html', {'guide': guide})
+    guide = Guide.objects.filter(header=header.capitalize())[0]
+    html = markdown(guide.content)
+    return render(request, 'guides/guide.html', {'guide': guide, 'html': html})
 
