1b31a994163b49c8a77f56f326b2dd4ca765cb49
Eerik Sven Puudist <eerik.sven@hotmail.com>
2018-11-15 13:04:16 +0200

    Add test for dbbackup cpmmand

--- src/general/tests.py
+++ src/general/tests.py
@@ -1,3 +1,19 @@
+import subprocess
+import os
+import re
 from django.test import TestCase
+from django.conf import settings
+from utils.opsys import change_dir
 
-# Create your tests here.
+OUTPUT_REGEX = re.compile(r"Backup 'db-backup-at-(\d{4}-\d{2}-\d{2})-\d{6}.sqlite3' was created successfully")
+
+
+class ManagePyCommandsTest(TestCase):
+
+    def test_dbbackup_command(self):
+        num_of_db_backups = os.listdir(settings.BACKUP_DIR)
+        p = subprocess.run('python ./manage.py dbbackup',
+                           stdout=subprocess.PIPE, shell=True)
+        self.assertTrue(OUTPUT_REGEX.match(p.stdout.decode().strip()))
+        new_num_of_db_backups = os.listdir(settings.BACKUP_DIR)
+        self.assertEqual(len(num_of_db_backups) + 1, len(new_num_of_db_backups))
